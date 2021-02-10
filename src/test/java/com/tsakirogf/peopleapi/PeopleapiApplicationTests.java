1ebb378a26f213ef6a46a27d7b4242db409dac30
Fotis Tsakiroglou <tsakirogf@hotmail.com>
2021-02-10 18:14:43 +0000

    Initial commit

---
+++ src/test/java/com/tsakirogf/peopleapi/PeopleapiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tsakirogf.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleapiApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
