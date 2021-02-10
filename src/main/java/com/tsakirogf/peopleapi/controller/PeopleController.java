7b55cd86ac3eeff684ba663ebaa751e2b5e41104
Fotis Tsakiroglou <tsakirogf@hotmail.com>
2021-02-10 22:00:25 +0000

    [DEV] Add controller and mappings

--- src/main/java/com/tsakirogf/peopleapi/controller/PeopleController.java
+++ src/main/java/com/tsakirogf/peopleapi/controller/PeopleController.java
@@ -1,2 +1,27 @@
-package com.tsakirogf.peopleapi.controller;public class PeopleController {
+package com.tsakirogf.peopleapi.controller;
+
+import com.tsakirogf.peopleapi.model.People;
+import com.tsakirogf.peopleapi.service.PeopleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("app/people")
+public class PeopleController
+{
+    @Autowired
+    private PeopleServiceImpl peopleService;
+
+    public PeopleController(PeopleServiceImpl peopleService)
+    {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping(value = { "/app/people"})
+    public Iterable<People> getListOfPeople()
+    {
+        return peopleService.getListOfPeople();
+    }
 }
