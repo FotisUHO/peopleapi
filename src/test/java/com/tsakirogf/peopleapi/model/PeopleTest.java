04ff642e7d9e4faab4a7ece8d533f7ddd57c5616
Fotis Tsakiroglou <tsakirogf@hotmail.com>
2021-02-12 20:09:10 +0000

    [TEST] add unitesting for the model

--- src/test/java/com/tsakirogf/peopleapi/model/PeopleTest.java
+++ src/test/java/com/tsakirogf/peopleapi/model/PeopleTest.java
@@ -6,6 +6,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PeopleTest
 {
+    @Test
+    void testContructor()
+    {
+        People testPeople = new People(uninderestingTestValues.Id,
+                                       uninderestingTestValues.Name,
+                                       uninderestingTestValues.Age,
+                                       uninderestingTestValues.Balance,
+                                       uninderestingTestValues.Email,
+                                       uninderestingTestValues.Address);
+        assertEquals(uninderestingTestValues.Name, testPeople.getName());
+        assertEquals(uninderestingTestValues.Age, testPeople.getAge());
+        assertEquals(uninderestingTestValues.Balance, testPeople.getBalance());
+        assertEquals(uninderestingTestValues.Email, testPeople.getEmail());
+        assertEquals(uninderestingTestValues.Address, testPeople.getAddress());
+    }
+
+    @Test
+    void testSetGetName()
+    {
+        People testSubject = TestPeopleFactory();
+        final String testName = "A random name";
+        testSubject.setName(testName);
+        assertEquals(testName, testSubject.getName());
+    }
+
+    @Test
+    void testSetGetAge()
+    {
+        People testSubject = TestPeopleFactory();
+        final int testAge = 607;
+        testSubject.setAge(testAge);
+        assertEquals(testAge, testSubject.getAge());
+    }
+
+    @Test
+    void testSetGetBalance()
+    {
+        People testSubject = TestPeopleFactory();
+        final double testBalance = 12.01;
+        testSubject.setBalance(testBalance);
+        assertEquals(testBalance, testSubject.getBalance());
+    }
+
+    @Test
+    void testSetGetEmail()
+    {
+        People testSubject = TestPeopleFactory();
+        final String testMail = "A random email";
+        testSubject.setEmail(testMail);
+        assertEquals(testMail, testSubject.getEmail());
+    }
+
+    @Test
+    void testSetGetAddress()
+    {
+        People testSubject = TestPeopleFactory();
+        final String testAddress = "A random address";
+        testSubject.setAddress(testAddress);
+        assertEquals(testAddress, testSubject.getAddress());
+    }
+
     class TestValues
     {
         long Id;
@@ -36,7 +97,8 @@ class PeopleTest
 
     People TestPeopleFactory()
     {
-        return new People(uninderestingTestValues.Name,
+        return new People(uninderestingTestValues.Id,
+                          uninderestingTestValues.Name,
                           uninderestingTestValues.Age,
                           uninderestingTestValues.Balance,
                           uninderestingTestValues.Email,
